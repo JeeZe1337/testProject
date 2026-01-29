3b70339716d655a337a8826db0bf7107a67f1996
jeeze1337 <dakak99@yandex.ru>
2026-01-30 03:15:19 +0700

    add attach player and damage, new kill enemy

--- scene/enemy/ninja_enemy.gd
+++ scene/enemy/ninja_enemy.gd
@@ -6,12 +6,12 @@ const JUMP_VELOCITY = -450.0
 var current_speed = SPEED_WALK
 var chase = false
 @onready var anim = $AnimationNinja
+@onready var player = $"../../player/player"
 var alive = true
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	var player = $"../../player/player"
 	var direction = (player.position - self.position).normalized()
 	if alive == true:
 		if chase == true:
@@ -44,10 +44,20 @@ func _on_damage_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		if alive == true:
 			body.health -= 1
-		
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body.name == "Map":
+		death()
+
 func death():
 	alive = false
 	velocity.x = 0
 	anim.play("dead")
 	await anim.animation_finished
 	queue_free()
+
+func _on_damage_area_entered(area: Area2D) -> void:
+	if area.name == "Attach":
+		player.damage_enemy = true
+	if player.damage_enemy == true and player.anim_damage_enemy == true:
+			death()
--- scene/player/player.gd
+++ scene/player/player.gd
@@ -3,47 +3,60 @@ extends CharacterBody2D
 const SPEED_WALK = 100.0
 const SPEED = 250.0
 const JUMP_VELOCITY = -450.0
+const LIMIT_FALL = 1200.0
 var current_speed = SPEED_WALK
 var health = 5
 var alive = true
 var damage_area = null
+var attach = false
+var damage_enemy = false
+var anim_damage_enemy = false
 @onready var anim = $AnimatedSprite2D
+@onready var ninjaEnemy = $"../../enemy/ninja_enemy"
 
 func _physics_process(delta: float) -> void:
-	var attach = false
 	var is_running = Input.is_action_pressed("shift")
 	current_speed = SPEED if is_running else SPEED_WALK
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	if alive == true:
+	if Input.is_action_just_pressed("left_click"):
+		attach = true
+		if velocity.y == 0:
+			velocity.x = 0
+		anim.play("attack_one")
+		await anim.animation_finished
+		attach = false
+		anim_damage_enemy = false
+		$Attach/CollisionAttach.set_deferred("disabled", true)
+	if alive == true and attach == false:
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			anim.play("jump")
 		var direction := Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * current_speed
-			if velocity.y == 0 && is_running && attach == false:
+			if velocity.y == 0 && is_running:
 				anim.play("run")
-			elif velocity.y == 0 && attach == false:
+			elif velocity.y == 0:
 				anim.play("walk")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED_WALK)
-			if velocity.y == 0 && attach == false:
+			if velocity.y == 0:
 				anim.play("idle")
-		if Input.is_action_just_pressed("left_click"):
-			attach = true
-			anim.play("attack_one")
 		if direction == -1:
-			$AnimatedSprite2D.flip_h = true
+			$Attach.scale.x = -1
+			anim.flip_h = true
 		elif direction == 1:
-			$AnimatedSprite2D.flip_h = false
+			$Attach.scale.x = 1
+			anim.flip_h = false
 		if velocity.y > 0:
 			anim.play("fall")
 		if $Inv.time_left == 0:
 			if damage_area != null:
 				take_damage()
 				$Inv.start()
+		if position.y > LIMIT_FALL:
+			death()
 		
 	if health <= 0:
 		death()
@@ -66,3 +79,8 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 func _on_hit_box_body_exited(body: Node2D) -> void:
 	if damage_area == body:
 		damage_area = null
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if $AnimatedSprite2D.animation == "attack_one" and $AnimatedSprite2D.frame == 4:
+		$Attach/CollisionAttach.set_deferred("disabled", false)
+		anim_damage_enemy = true
